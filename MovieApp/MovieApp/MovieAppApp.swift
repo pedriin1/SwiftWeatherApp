84887b7fda47152f9cd09b6bc3396f8ec9be0565
frigopedro <46055473+pedriin1@users.noreply.github.com>
2021-11-19 17:07:51 -0300

    First commit

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Pedro Maia on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
